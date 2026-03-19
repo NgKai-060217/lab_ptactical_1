9f55224eb63d617b7a6a39d322ad0d3d639dfe9b
NgKai-060217 <ngjunkai0387@gmail.com>
2026-03-19 17:04:27 +0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'screens/input_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue), // 换成蓝色主题
+      home: const InputScreen(),
+    );
+  }
+}
